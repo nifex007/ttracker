57ddbd65fec013b2dd0674b7f3a83bb4185b66bb
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-02-23 23:59:12 +0100

    style: Correcting typos in hardcoded tasks list

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,26 @@ const tasks = [
         id: 1,
         text: 'Call Bae',
         dat: 'Feb 27th at 2:30pm',
-        remember: true
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Check on Chinwe',
+        dat: 'March 1st at 2:30pm',
+        reminder: true
+        
+    },
+    {
+        id: 3,
+        text: 'Call Mom',
+        date: 'March 27th at 2:30pm',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: 'Check if sister is coming home',
+        date: 'Everyday',
+        reminder: true
     }
 
 ];
