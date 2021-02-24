fea4087d504b4484e4e91b2eeaec165116e5af4d
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-02-25 00:00:13 +0100

    refactor: Moved tasks into a gobal state as being passed as props to the tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,34 +1,4 @@
-import { useState } from 'react'
-
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Call Bae',
-            date: 'Feb 27th at 2:30pm',
-            reminder: true
-        },
-        {
-            id: 2,
-            text: 'Check on Chinwe',
-            date: 'March 1st at 2:30pm',
-            reminder: true
-            
-        },
-        {
-            id: 3,
-            text: 'Call Mom',
-            date: 'March 27th at 2:30pm',
-            reminder: false
-        },
-        {
-            id: 4,
-            text: 'Check if sister is coming home',
-            date: 'Everyday',
-            reminder: true
-        }
-    
-    ])
+const Tasks = ({tasks}) => {
     return (
         <div>
             { tasks.map((task) => (
