1143f5ffd38ba76d034c279d220d620cef0a87ba
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-02-24 23:54:55 +0100

    refactor: Moved tasks list to state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,34 +1,34 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Call Bae',
-        date: 'Feb 27th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Check on Chinwe',
-        date: 'March 1st at 2:30pm',
-        reminder: true
-        
-    },
-    {
-        id: 3,
-        text: 'Call Mom',
-        date: 'March 27th at 2:30pm',
-        reminder: false
-    },
-    {
-        id: 4,
-        text: 'Check if sister is coming home',
-        date: 'Everyday',
-        reminder: true
-    }
-
-];
-
+import { useState } from 'react'
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Call Bae',
+            date: 'Feb 27th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Check on Chinwe',
+            date: 'March 1st at 2:30pm',
+            reminder: true
+            
+        },
+        {
+            id: 3,
+            text: 'Call Mom',
+            date: 'March 27th at 2:30pm',
+            reminder: false
+        },
+        {
+            id: 4,
+            text: 'Check if sister is coming home',
+            date: 'Everyday',
+            reminder: true
+        }
+    
+    ])
     return (
         <div>
             { tasks.map((task) => (
